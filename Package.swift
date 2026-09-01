fae5df491192bb93bb9c82d3ceb4bc85419eb790
fpcmotif <fpcmotif@gmail.com>
2026-09-01 16:08:36 +0800

    chore(deps): adopt fff-swift 0.2.1 (#51)

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/vmg-dev/fff-swift",
-            from: "0.2.0"
+            from: "0.2.1"
         ),
     ],
     targets: [
